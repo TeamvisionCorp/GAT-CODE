320a5d111fc6b4a4a87a739b8f35469ae92accf3
iquality <155174391@qq.com>
2017-08-22 17:22:34 +0800

    添加自动化用例model文件

---
+++ GATCore/src/main/java/com/gateside/autotesting/Gat/dataobject/testcase/AutoTestCase.java
@@ -0,0 +1,38 @@
+package com.gateside.autotesting.Gat.dataobject.testcase;
+
+import org.apache.xmlbeans.impl.xb.xsdschema.Public;
+
+import com.google.gson.Gson;
+
+public class AutoTestCase {
+	
+	public Integer id;
+	
+	public String PackageName;
+	
+	public String ClassName;
+	
+	public String CaseName;
+	
+	public Integer CaseType;
+	
+	public Integer ProjectID;
+	
+	public Integer ModuleID;
+	
+	public Integer InterfaceID;
+	
+	public String CaseTag;
+	
+	public Integer Version;
+	
+	public String Desc;
+	
+	
+	public String toJson()
+	{
+		Gson gsonObject=new Gson();
+		return gsonObject.toJson(this);
+	}
+
+}
